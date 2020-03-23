14e76b7ef2a48c107cac0860c50637dc9e428582
Хайрулло <khairullo@MacBook-Pro-Hajrullo.local>
2020-03-23 15:34:54 +0100

    First commit and last :)

---
+++ src/Chat.php
@@ -0,0 +1,63 @@
+<?php
+namespace MyApp;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        // Store the new connection to send messages to later
+        $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+            //My modification        
+            $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
+            fwrite($myfile, $msg);
+            fclose($myfile);
+            //My modification
+
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onBinaryMessage(ConnectionInterface $from, $msg) {
+        // There binary message
+        // $hexString = '1200003456';
+        //     $bin = hex2bin($hexString);
+        //     echo 'Len is: ' . strlen($bin);
+        //     $hex = bin2hex($bin);
+        //     echo 'Hex is: ' . $hex;
+
+        $data = "";
+        $from->sendBinary($data);
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        // The connection is closed, remove it, as we can no longer send it messages
+        $this->clients->detach($conn);
+
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error has occurred: {$e->getMessage()}\n";
+
+        $conn->close();
+    }
+}
